207c607e5bc3ecc00ee8f3c8a3aaeec4c9e74eb2
MichaelAraya0210 <Michaelaraya0210@users.noreply.github.com>
2019-10-01 11:02:51 -0700

    an attempt

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-lambdata-MichaelAraya - a collection of data science helper functions for lambda school.
+lambdata - a collection of data science helper functions for lambda school
 """
 import setuptools
 ​
@@ -10,20 +10,19 @@ REQUIRED = [
 ​
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
-
-setuptools.setup(
-    name="lambdata-MichaelAraya",
+    setuptools.setup(
+    name="lambdata-alekslovesdata",
     version = "0.1.1",
-    author = "MichaelAraya",
+    author = "alekslovesdata",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/Michaelaraya0210/lambdata-MichaelAraya",
+    url="https://lambdaschool.com/courses/data-science",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires = REQUIRED,
     classifiers=["Programming Language :: Python :: 3",
-        "License::OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+    )
