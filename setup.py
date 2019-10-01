5163ab0b415456760b00d0826a38ff3b1f3923b6
MichaelAraya0210 <Michaelaraya0210@users.noreply.github.com>
2019-10-01 13:19:38 -0700

    random

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
-"""lambda-MichaelAraya is a collection of data science helper functions"""
-
+"""
+lambdata - a collection of data science helper functions for lambda school
+"""
 import setuptools
-
-REQUIRED = ["numpy", "pandas"]
-​
-with open("README.md", "r") as fh:    LONG_DESCRIPTION = fh.read()    setuptools.setup(    name = "lambdata-MichaelAraya",    version = "0.1.1",    author = "MichaelAraya",    description = "a colection of data science helper functions",    long_description = LONG_DESCRIPTION,    long_description_content_type = "text/markdown",    url = "https://lambdaschool.com/courses/data-science",    packages = setuptools.find_packages(),    python_requires = ">=3.5",    install_requires = REQUIRED,    classifiers = ["Programming Language :: Python :: 3",    "License :: OSI Approved :: MIT License",    "Operating System :: OS Independent"])
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    setuptools.setup(
+    name="lambdata_MichaelAraya",
+    version = "0.1.1",
+    author = "Michael araya",
+    description = "a collection of data science helper functions",
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://lambdaschool.com/courses/data-science",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires = REQUIRED,
+    classifiers=["Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+    )
