fb345e375a087b91dcf2ca803101ef60ca9660b5
Michaelaraya0210 <Michaelaraya0210@users.noreply.github.com>
2019-10-01 10:13:13 -0700

    try 10000

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 lambdata - a collection of data science helper functions for lambda school
 """
-import setuptools
+import setuptools,
 ​
 REQUIRED = [
     "numpy",
@@ -11,9 +11,9 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
-    name="lambdata-alekslovesdata",
+    name="lambdata-MichaelAraya",
     version = "0.1.1",
-    author = "alekslovesdata",
+    author = "MichaelAraya",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
